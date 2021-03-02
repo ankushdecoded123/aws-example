525bdad6ea7e7034ac583b345aa9509b48e7b37a
iankuduffo <dev.java.ankush@gmail.com>
2021-03-02 12:22:04 +0530

    first commit

---
+++ src/main/java/com/example/aws/controller/RestController.java
@@ -0,0 +1,17 @@
+package com.example.aws.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping(path = "/api")
+public class RestController {
+
+	@GetMapping("/call")
+	public ResponseEntity<String> helloAWS()
+	{
+		return new ResponseEntity<>("Hello AWS, calling my EC-2 instance", HttpStatus.OK);
+	}
+}
